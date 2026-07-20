17ef08c9ab656a25fde6b225c729809cbdf3e27a
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-07-19 22:29:45 -0700

    Load cross-device music fixes on every page

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+  const response = await context.next();
+  const contentType = response.headers.get("content-type") || "";
+
+  if (!contentType.includes("text/html")) return response;
+
+  return new HTMLRewriter()
+    .on("body", {
+      element(element) {
+        element.append('<script src="/site-fixes.js?v=20260719" defer></script>', { html: true });
+      },
+    })
+    .transform(response);
+}
