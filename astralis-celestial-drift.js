a784550215e2c07c3e67fdb37ec9ca406a81793e
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-07-21 15:05:59 -0700

    Update floating worlds with recent exoplanet artwork

--- astralis-celestial-drift.js
+++ astralis-celestial-drift.js
@@ -1,14 +1,16 @@
-
 (function(){
-  if (document.getElementById('astralis-celestial-drift')) return;
-  const layer = document.createElement('div');
+  const existing = document.getElementById('astralis-celestial-drift');
+  const layer = existing || document.createElement('div');
+
   layer.id = 'astralis-celestial-drift';
   layer.setAttribute('aria-hidden', 'true');
   layer.innerHTML = `
-    <img class="celestial gas" src="astralis-gas-giant.png" alt="">
-    <img class="celestial earth" src="astralis-earth.png" alt="">
-    <img class="celestial mars" src="astralis-mars.png" alt="">
-    <img class="celestial comet" src="astralis-comet.png" alt="">
+    <img class="celestial beta" src="/recent-beta-pictoris-d.svg" alt="">
+    <img class="celestial toi" src="/recent-toi-1752-b.svg" alt="">
+    <img class="celestial wispit" src="/recent-wispit-2b.svg" alt="">
+    <img class="celestial gaia" src="/recent-gaia23bra-b.svg" alt="">
+    <img class="celestial comet" src="/comet-c2026-a1-maps.svg" alt="">
   `;
-  document.body.prepend(layer);
+
+  if (!existing) document.body.prepend(layer);
 })();
