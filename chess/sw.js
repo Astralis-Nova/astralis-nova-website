d26d779338e25ce0ba4a896236917cfed3fd8ecb
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-08-02 02:00:01 -0700

    Refresh chess cache for direct move controls

--- chess/sw.js
+++ chess/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'astralis-nova-chess-v13';
+const CACHE = 'astralis-nova-chess-v14';
 const ASSETS = ['./','./index.html','./chess.css','./chess.js','./move-guide.js','./trideck-engine.js','./pieces.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
