bd9e7219bcc21a288a2b9edb9fedb289b2ae537b
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-08-02 01:52:58 -0700

    Refresh chess cache for move guidance

--- chess/sw.js
+++ chess/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'astralis-nova-chess-v12';
-const ASSETS = ['./','./index.html','./chess.css','./chess.js','./trideck-engine.js','./pieces.js','./icon.svg','./manifest.webmanifest'];
+const CACHE = 'astralis-nova-chess-v13';
+const ASSETS = ['./','./index.html','./chess.css','./chess.js','./move-guide.js','./trideck-engine.js','./pieces.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
 ));
