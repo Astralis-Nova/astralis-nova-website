e2313bce09f880bd5a592498a0bb8d48fd60df1e
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-08-04 09:45:13 -0700

    Verify and correct all Quote Orbit attributions

--- quote-attribution-audit.js
+++ quote-attribution-audit.js
@@ -1,5 +1,2 @@
 (()=>{
-  const records=[
-    {
-      match:'Remember to look up at the stars and not down at your feet.',
-      alternate:'Remember to look up at the stars and not down at your feet. Try to make sense of what you see and wonder about what makes the universe exist. Be curious. And however difficult life may
+  const hawkingFull='Remember to look up at the stars and not down at your feet. Try to make sense of what you see and wonder about what makes the universe exist. Be curious. And however difficult life may
