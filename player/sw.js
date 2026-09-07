b30f324792d31e757b640d025ccd657ef8e161ad
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-09-07 13:41:31 -0700

    Purge player planet visual from PWA shell cache

--- player/sw.js
+++ player/sw.js
@@ -1,6 +1,6 @@
-const SHELL_CACHE='astralis-nova-player-v12';
+const SHELL_CACHE='astralis-nova-player-v13';
 const AUDIO_CACHE='astralis-nova-offline-audio-v1';
-const SHELL=['./','./index.html','./player.css','./player.js','./folder-library.js','./local-library.js','./planet-orbit.js','./folder-library-core.js','./download-access.js','./first-run-folder.js','./legacy83-components.css','./legacy83-enhancements.js','./legacy83-radio.js','./legacy83-radio-memory.js','./legacy83-kmle-fallback.js','./manifest.webmanifest','./icon.svg','../cover-3.jpg','../cover-16.jpg','../astralis-gas-giant.png'];
+const SHELL=['./','./index.html','./player.css','./player.js','./folder-library.js','./local-library.js','./folder-library-core.js','./download-access.js','./first-run-folder.js','./legacy83-components.css','./legacy83-enhancements.js','./legacy83-radio.js','./legacy83-radio-memory.js','./legacy83-kmle-fallback.js','./manifest.webmanifest','./icon.svg','../cover-3.jpg','../cover-16.jpg'];
 
 self.addEventListener('install',event=>event.waitUntil(
   caches.open(SHELL_CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())
