0d8967776cf613081326856b706972dff2a8e3c1
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-09-08 14:24:55 -0700

    Load Nova owner console and knowledge editor

--- nova-guide.js
+++ nova-guide.js
@@ -26,6 +26,7 @@
     loadScript('/nova-autonomy.js?v=20260908a','novaAutonomy');
     loadScript('/nova-personality-sparks.js?v=20260908a','novaPersonalitySparks');
     loadScript('/nova-capabilities.js?v=20260908a','novaCapabilities');
+    loadScript('/nova-owner-console.js?v=20260908a','novaOwnerConsole');
     loadScript('/nova-home-primary.js?v=20260908a','novaHomePrimary');
     loadScript('/feeling-tipsy.js?v=20260805a','feelingTipsy');
   };
