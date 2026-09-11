df43a89dc75ca7db0b47ddaeb808a93b286e71a8
Ramon Bivens <30357438+Astralis-Nova@users.noreply.github.com>
2026-09-10 21:42:18 -0700

    Reset stale chess state before Tri-D engine loads

---
+++ chess/trid-fresh-start.js
@@ -0,0 +1,8 @@
+(()=>{
+'use strict';
+const MARKER='astralis-trid-fresh-start-v2';
+const GAME_KEY='astralisTriDGameV2';
+if(localStorage.getItem(MARKER))return;
+localStorage.removeItem(GAME_KEY);
+localStorage.setItem(MARKER,'1');
+})();
